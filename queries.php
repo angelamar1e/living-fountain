48983166c1eb6f1b75db863d74f6cf46c52b1626
angela <angela.delapena06@gmail.com>
2024-01-30 17:53:00 +0800

    added dropdown for deliverer

--- queries.php
+++ queries.php
@@ -20,4 +20,23 @@ function select($cols, $table){
     return $result;
 }
 
+function select_where($cols, $table, $condition){
+    global $conn;
+    $select = "SELECT ";
+    $col_count = 0;
+    // fetching column names from array passed
+    foreach ($cols as $col) {
+        $col_count += 1;
+        if ($col_count > 1){
+            $select .= ", ";
+        }
+        $select .= $col;
+    }
+    $query = "$select
+    FROM $table
+    WHERE $condition";
+    $result = mysqli_query($conn, $query);
+    return $result;
+}
+
 ?>
--- sales.php
+++ sales.php
@@ -2,10 +2,10 @@
     include("connection.php");
     include("queries.php");
     $all_products = select(array("code","product_desc"), "products");
+    $all_deliverers = select_where(array("id","employee_name"),"employees","emp_type_code = 'D'");
 ?>
 
 <!DOCTYPE html>
-
 <html lang=en>
     <body>
         <form>
@@ -19,12 +19,12 @@
 
             <h3>Order</h3>
             <label for="type">Type</label>
-            <label for="lot"> Lot</label>
-            <label for="ph">Phase</label><br>
+            <label for="lot"> Quantity</label>
+            <label for="ph"> Deliverer</label><br>
             <select id="prod_type" name="prod_type" required>
                 <!-- default option, hints no selection -->
                 <option value="" selected disabled hidden>Select</option>
-                <!-- fetching each prop type to be set as options, value is id but text displayed is the desc -->
+                <!-- fetching each prop type to be set as options, value is code but text displayed is the desc -->
                 <?php 
                     while($product = mysqli_fetch_array($all_products,MYSQLI_ASSOC)):;
                 ?> 
@@ -35,8 +35,21 @@
                     endwhile;
                 ?>
             </select>
-            <input type="text" id="lot" name="lot">
-            <input type="text" id="ph" name="phase">
+            <input type="text" id="qty" name="qty">
+            <select id="deliverer" name="deliverer" required>
+                <!-- default option, hints no selection -->
+                <option value="" selected disabled hidden>Select</option>
+                <!-- fetching each deliverer to be set as options, value is id but text displayed is the desc -->
+                <?php 
+                    while($deliverer = mysqli_fetch_array($all_deliverers,MYSQLI_ASSOC)):;
+                ?> 
+                    <option value="<?php echo $deliverer['id'];?>">
+                        <?php echo $deliverer['employee_name']; ?>  
+                    </option>
+                <?php
+                    endwhile;
+                ?>
+            </select>
         </form>
     </body>
 </html>
