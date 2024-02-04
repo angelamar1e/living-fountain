e2dff2a8aa4d703a3696a07e5333233668883167
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-02-04 09:36:23 +0800

    dateForm submission and processing orders

--- revenue.php
+++ revenue.php
@@ -2,4 +2,19 @@
     include("connection.php");
     include("queries.php");
     include("alerts.php");
+    
+// handling dateForm submission and processing orders
+
+    if (isset($_REQUEST['weekly_tab'])) {
+        // Display Weekly Revenue
+        $start_date = date('Y-m-d', strtotime('last Monday'));
+        $end_date = date('Y-m-d', strtotime('this Sunday'));
+        $weekly_revenue = get_weekly_revenue($start_date, $end_date);
+    } elseif (isset($_REQUEST['monthly_tab'])) {
+        // Display Monthly Revenue
+        $month = date('m');
+        $year = date('Y');
+        $monthly_revenue = get_monthly_revenue($month, $year);
+    }
+
 ?>
