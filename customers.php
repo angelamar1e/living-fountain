0dda84a9a4c9363cb334bf56dbbe7ae178cfefc6
angela <angela.delapena06@gmail.com>
2024-02-06 15:42:26 +0800

    changed cursor to a pointer for clickable td

--- customers.php
+++ customers.php
@@ -92,7 +92,7 @@
                                             $block = $record['block'];
                                             $lot = $record['lot'];
                                             $phase = $record['phase']; ?>
-                                            <tr class="text-center" onclick="location.href='customer_activity.php?block=<?php echo $block; ?>&lot=<?php echo $lot; ?>&phase=<?php echo $phase; ?>'">
+                                            <tr class="text-center" style="cursor:pointer;" onclick="location.href='customer_activity.php?block=<?php echo $block; ?>&lot=<?php echo $lot; ?>&phase=<?php echo $phase; ?>'">
                                                 <td><?php echo $block; ?> </td>
                                                 <td><?php echo $lot; ?> </td>
                                                 <td><?php echo $phase; ?> </td>
