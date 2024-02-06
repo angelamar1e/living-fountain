80015312125280bcf05a1f3965fa8c1c7ca00b0e
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-02-06 11:51:07 +0800

    salarycss_fe revision

--- salary.php
+++ salary.php
@@ -35,8 +35,8 @@ $additional_amount = get_additional();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Salary</title>
-    <link rel="stylesheet" href="salary.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="salary.css">
 </head>
 <style>
     .emp_type {
