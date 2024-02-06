9fabd792ebebebc1f0274442612493133ac46c79
Gelo <110366154+neilangeloespinoza@users.noreply.github.com>
2024-02-07 01:47:06 +0800

    Update customer_activity.php

--- customer_activity.php
+++ customer_activity.php
@@ -19,6 +19,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="customer_activity.css">
         <title>Block <?php echo $block," Lot ", $lot," Phase ", $phase ?></title>
         <style>
         @font-face {
